9c0d094de0537b6a45e5a9183089cb01a598ef01
Diego Rodriguez <39448880+diegorod9000@users.noreply.github.com>
2019-08-23 12:05:00 -0500

    implement delete item funcionality (#2)
    * implement delete item funcionality
    
    * Rebased from master

--- ToDo.java
+++ ToDo.java
@@ -33,6 +33,9 @@ class ToDo
                 if(src==addButton)
                     if (!newCheck.getText().equals(""))
                         addBox(newCheck.getText());
+                for (int x=0; x<deletes.size();x++)
+                    if(deletes.get(x)==src)
+                        delete(x);
             }
         };
         frame=new JFrame("ToDo");
@@ -55,11 +58,23 @@ class ToDo
         frame.pack();
     }
 
+    public void delete (int x)
+    {
+        checkPanel.remove(deletes.get(x));
+        checkPanel.remove(boxes.get(x));
+        boxes.remove(x);
+        deletes.remove(x);
+        frame.remove(checkPanel);
+        frame.add(checkPanel, BorderLayout.LINE_START);
+        frame.pack();
+    }
+
     private void addDeleteButton(JCheckBox checkbox)
     {
         JButton deleter=new JButton("delete");
         deleter.setLocation(checkbox.getX()+500, checkbox.getY());
         deleter.setSize(1,1);
+        deleter.addActionListener(checker);
         checkPanel.add(deleter);
         deletes.add(deleter);
     }
@@ -69,7 +84,6 @@ class ToDo
         int x =boxes.size();
         boxes.add(new JCheckBox(input));
         boxes.get(x).setActionCommand(input);
-        boxes.get(x).addActionListener(checker);
         checkPanel.add(boxes.get(x));
         addDeleteButton(boxes.get(x));
         frame.remove(checkPanel);
